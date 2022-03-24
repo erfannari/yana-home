222c48702a1fe36c4bbd1f93fd91f1267d9c704a
erfan narshirin <erfan98nari@gmail.com>
2022-03-24 05:32:42 +0430

    add tranding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './components/login/login.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { InnerCategoriesComponent } from './components/store/categories/inner-categories/inner-categories.component';
+import { TrendingComponent } from './components/store/trending/trending.component';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { InnerCategoriesComponent } from './components/store/categories/inner-ca
     LoginComponent,
     HeroComponent,
     SignUpComponent,
-    InnerCategoriesComponent
+    InnerCategoriesComponent,
+    TrendingComponent
   ],
   imports: [
     BrowserModule ,
--- src/app/components/store/categories/inner-categories/inner-categories.component.ts
+++ src/app/components/store/categories/inner-categories/inner-categories.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inner-categories',
   templateUrl: './inner-categories.component.html',
-  styleUrls: ['./inner-categories.component.scss']
+  styleUrls: ['./inner-categories.component.scss'],
 })
 export class InnerCategoriesComponent implements OnInit {
+  innerCategories: any[] = [
+    {
+      id: 1,
+      categoryName: 'Cushions',
+      categoryDescription: `خودت را با بهترین ها \n به یک لحظه خوش دعوت کن`,
+      image: '../../../../assets/images/optimizes-images/cushion2.jpg',
+    },
+    {
+      id: 1,
+      categoryName: 'Curtain',
+      categoryDescription: `شما شاهکار زندگی خودتون هستید
+      پس رسیدگی به روح نازنینتون رو فراموش کنید`,
+      image: '../../../../assets/images/optimizes-images/Curtain1.jpg',
+    },
+    {
+      id: 1,
+      categoryName: 'Kitchen',
+      categoryDescription: 'خودت را با بهترین ها به یک لحظه خوش دعوت کن',
+      image: '../../../../assets/images/optimizes-images/plate.jpg',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
