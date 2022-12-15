d7788d67ac56bbaa475a8ac773987959f1967cc5
erfan narshirin <erfan98nari@gmail.com>
2022-12-15 14:20:03 +0330

    last updated on dec 15 2:19pm

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,32 +10,44 @@ export class HeaderComponent implements OnInit {
     {
       id: 1,
       name: 'دسته بندی ها',
-      iconUrl: '../../../assets/icons/header/hamburger-menu-svgrepo-com.svg'
+      iconUrl: '../../../assets/icons/header/hamburger-menu-svgrepo-com.svg',
+      navRoute: "/login"
+
     },
     {
       id: 2,
       name: 'تخفیف ها و پیشنهاد ها',
-      iconUrl: '../../../assets/icons/header/discount-svgrepo-com.svg'
+      iconUrl: '../../../assets/icons/header/discount-svgrepo-com.svg',
+      navRoute: "/login"
+
     },
     {
       id: 3,
       name: 'شگفت انگیز ها',
-      iconUrl: '../../../assets/icons/header/discount2.svg'
+      iconUrl: '../../../assets/icons/header/discount2.svg',
+      navRoute: "/login"
+
     },
     {
       id: 4,
       name: 'درباره ی یانا هوم',
-      iconUrl: '../../../assets/icons/header/info-svgrepo-com.svg'
+      iconUrl: '../../../assets/icons/header/info-svgrepo-com.svg',
+      navRoute: "/login"
+
     },
     {
       id: 5,
       name: 'ورود / ثبت نام',
-      iconUrl: '../../../assets/icons/header/user-svgrepo-com.svg'
+      iconUrl: '../../../assets/icons/header/user-svgrepo-com.svg',
+      
+      navRoute: "/login"
     },
     {
       id: 6,
       name: 'سبد خرید',
-      iconUrl: '../../../assets/icons/header/shopping-cart-svgrepo-com.svg'
+      iconUrl: '../../../assets/icons/header/shopping-cart-svgrepo-com.svg',
+      navRoute: "/shoppingCart"
+
     },
   ];
 
